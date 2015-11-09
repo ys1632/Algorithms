b7620186e82ea081f83d80ff18337df2d698f02d
ys1632 <ys1632@nyu.edu>
2015-11-08 19:54:53 -0500

    Sunday commit

---
+++ Solution_301.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution_301 {
+    public static void main(String[] args) {
+        Solution_301 sol = new Solution_301();
+        System.out.println(sol.removeInvalidParentheses("()())()"));
+    }
+
+    public List<String> removeInvalidParentheses(String s) {
+        Queue<String> q = new LinkedList<String>();
+        HashSet<String> visited = new HashSet<String>();
+        List<String> res = new ArrayList<String>();
+        
+        if (s == null || s == "") {
+            res.add(s);
+            return res;
+        }
+        
+        q.add(s);
+        visited.add(s);
+        boolean found = false;
+        while(!q.isEmpty()) {
+            String str = q.poll();
+            if (isValid(str)) {
+                res.add(str);
+                found = true;
+            }
+            
+            if (found) continue;
+            
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != '(' && str.charAt(i) != ')') continue;
+                String str2 = str.substring(0, i) + str.substring(i+1);
+                if (visited.contains(str2)) continue;
+                q.add(str2);
+                visited.add(str2);
+            }
+        }
+        
+        return res;
+        
+    }
+    
+    public boolean isValid(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') count++;
+            if (s.charAt(i) == ')') count--;
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+}
